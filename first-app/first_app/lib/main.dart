c9dcccfdb1f8da2fa7a75f81474b10ec599d8494
Eric Armstrong <urkstrong@gmail.com>
2023-12-26 11:34:13 -0500

    add favorites page

--- first-app/first_app/lib/main.dart
+++ first-app/first_app/lib/main.dart
@@ -63,7 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -105,6 +105,34 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         );
       }
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text('You have '
+            '${appState.favorites.length} favorites:'),
+        ),
+        for (var fav in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(fav.asLowerCase),
+        ),
+      ]
     );
   }
 }
